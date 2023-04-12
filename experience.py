9a12f7c7028e669ec3d904e933632bfb5545c95b
onetwothr1 <kimjh1101@snu.ac.kr>
2023-04-12 21:01:23 +0900

    augmentation by flip

--- experience.py
+++ experience.py
@@ -14,7 +14,7 @@ def combine_experience(experience1, experience2):
     combined.mcts_probs = combined_mcts_probs
     return combined
 
-def combine_saved_experience(saved_experiences: list, save_path):
+def combine_saved_experiences(saved_experiences: list, save_path):
     states_list = []
     rewards_list = []
     mcts_probs_list = []
@@ -39,21 +39,29 @@ def combine_saved_experience(saved_experiences: list, save_path):
     combined.save_experience(save_path)
     print("%d experiences combined and saved" %len(combined.states))
     
-def rotate_augmentaion(experience, board_size):
-    rotated_states = [experience.states]
-    rotated_mcts_probs = [experience.mcts_probs]
-    for i in range(1, 4):
-        rotated_states.append(torch.rot90(experience.states , i, [2,3]))
-
-        # convert flat-tensor into a 2D board, rotate it, back to flat.
+def augmentation(experience, board_size):
+    augmented_states = []
+    augmented_mcts_probs = []
+    for i in range(0, 4):
+        # rotate
+        rotated_states = torch.rot90(experience.states , i, [2,3])
         mcts_probs_2d = np.reshape(experience.mcts_probs, (len(experience), board_size, board_size))
-        rotated = torch.rot90(mcts_probs_2d, i, [1,2])
-        flat = np.reshape(rotated, (len(rotated), -1))
-        rotated_mcts_probs.append(flat)
+        rotated_mcts_probs_2d = torch.rot90(mcts_probs_2d, i, [1,2])
+        rotated_mcts_probs_flat = np.reshape(rotated_mcts_probs_2d, (len(rotated_mcts_probs_2d), -1)) # back to flat tensor
+
+        # flip horizontally
+        flipped_states = torch.flip(rotated_states, dims=[2])
+        flipped_mcts_probs_2d = torch.flip(rotated_mcts_probs_2d, dims=[1])
+        flipped_mcts_probs_flat = np.reshape(flipped_mcts_probs_2d, (len(flipped_mcts_probs_2d), -1))
+        
+        augmented_states.append(rotated_states)
+        augmented_states.append(flipped_states)
+        augmented_mcts_probs.append(rotated_mcts_probs_flat)
+        augmented_mcts_probs.append(flipped_mcts_probs_flat)
 
     augmented = ExperienceCollector()
-    augmented.states = torch.cat(rotated_states, dim=0)
-    augmented.mcts_probs = torch.cat(rotated_mcts_probs, dim=0)
+    augmented.states = torch.cat(augmented_states, dim=0)
+    augmented.mcts_probs = torch.cat(augmented_mcts_probs, dim=0)
     augmented.rewards = experience.rewards.repeat(4)
     return augmented
 
